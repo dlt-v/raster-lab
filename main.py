2d3e7c41a3a6a622f33b9b2db953bcffba56f96d
Tomek Michalski <tmm.michalski@gmail.com>
2022-03-20 17:44:38 +0100

    add importing images in new window

--- main.py
+++ main.py
@@ -1,25 +1,43 @@
-from tkinter import Button, Label, PhotoImage, Tk, filedialog
+import tkinter as tk
+from tkinter import filedialog
 from PIL import Image, ImageTk
 
-root = Tk()
-root.geometry("300x50")
+root = tk.Tk()
+root.geometry("300x300")
 root.title("RasterLab")
-icon = PhotoImage(file='icon.png')
+icon = tk.PhotoImage(file='icon.png')
 root.iconphoto(False, icon)
 
 
 def import_image():
     extensions = [('formats', ['.jpg', '.png', 'bmp'])]
     file_path = filedialog.askopenfilename(filetypes=extensions)
-    label = Label(root, text=file_path)
-    label.pack()
+    new_img = ImageTk.PhotoImage(Image.open(file_path))
+    # label = tk.Label(root, text=file_path)
+    # label.pack()
+
+    new_window = tk.Toplevel(root)
+    new_window.iconphoto(False, icon)
+    new_window.title(file_path)
+
+    new_window.geometry("300x300")
+
+    image = tk.Label(new_window, image=new_img)
+    image.image = new_img
+    image.pack()
 
 
-import_button = Button(root,
-                       text="import image",
-                       pady=50,
-                       padx=50, command=import_image
-                       )
+import_button = tk.Button(root,
+                          text="import image",
+                          pady=50,
+                          padx=50, command=import_image
+                          )
 import_button.pack()
+
+# img = tk.PhotoImage(file='icon.png')
+# tk.Label(
+#     root,
+#     image=img
+# ).pack()
 
 root.mainloop()
