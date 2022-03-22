d4323a434b0cca6ea497278a404f980648b237cc
Tomek Michalski <tmm.michalski@gmail.com>
2022-03-22 17:21:19 +0100

    add plot profile functionality

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import tkinter as tk
 from tkinter import filedialog
-from typing import Any, Dict
+from typing import Dict, List
 from PIL import Image, ImageTk
 import matplotlib.pyplot as plt
-import matplotlib as mtl
+import matplotlib as mpl
+import numpy as np
+import scipy.ndimage
 
 
 def terminate_all():
+    """
+    When you close the root element, destroy all the matplotlib figures as well and close the program.
+    """
     plt.close('all')
     root.destroy()
 
@@ -18,50 +23,111 @@ icon = tk.PhotoImage(file='icon.png')
 root.iconphoto(False, icon)
 root.resizable(False, False)
 root.protocol("WM_DELETE_WINDOW", terminate_all)
-mtl.rcParams['toolbar'] = 'None'
+mpl.rcParams['toolbar'] = 'None'
 focused_file: Dict[str, str] = {
     "path": "",
     "mode": ""
 }
+plot_profile_data: Dict[str, List[int]] = {
+    "start": [-1, -1],
+    "end": [-1, -1]
+}
 
 
 def import_image():
+    """
+    Asks for the image in file explorer and then imports it to the program, creating a dedicated widget for it.
+    """
     extensions = [('formats', ['.jpg', '.png', '.bmp', '.jpeg'])]
     file_path = filedialog.askopenfilename(filetypes=extensions)
     if not file_path:
         return
     opened_image = Image.open(file_path)
     new_img = ImageTk.PhotoImage(opened_image)
-    # label = tk.Label(root, text=file_path)
-    # label.pack()]
 
     new_window = tk.Toplevel(
         root, width=opened_image.width, height=opened_image.height)
     new_window.iconphoto(False, icon)
     new_window.title(f"RasterLab: {file_path}")
-    geometry = f"{opened_image.height}x{opened_image.width}"
-    print(f"Width is: {opened_image.height}\nHeight is: {opened_image.width}")
-    # new_window.geometry(geometry)
-    # new_window.wm_geometry(geometry)
-    # new_window.update()
     new_window.resizable(False, False)
 
     def on_focus(event):
+        """
+        Switches the focused_file data to the current image in order for other functions to operate on them. Resets the plot profile data since it's another image.
+        """
         global focused_file
-        print(f"Focused file changed from: {focused_file['path']}...")
+        global plot_profile_data
+
+        # reset plot profile data for new image
+        plot_profile_data["start"] = [-1, -1]
+        plot_profile_data["end"] = [-1, -1]
+        plot_profile_button["state"] = "disabled"
+
         focused_file["path"] = file_path
         focused_file["mode"] = opened_image.mode
-        print(f"...to: {focused_file['path']}.")
+        # print(f"Focused file changed to: {focused_file['path']}...")
         # enable buttons
         if focused_file["mode"] == 'L':
             histogram_array_button["state"] = "normal"
-            plot_profile_button["state"] = "normal"
         else:
             plot_profile_button["state"] = "disabled"
             histogram_array_button["state"] = "disabled"
         histogram_button["state"] = "normal"
 
+    def on_scroll(event):
+        """
+        Depending on the scroll direction it resizes the image up and down, scaling the entire widget/frame with it.
+        """
+        # get current widget height and width, account for borders (-4)
+        cur_height = new_window.winfo_height() - 4
+        cur_width = new_window.winfo_width() - 4
+        if event.delta == 120:
+            new_width = int(cur_width * 1.05)
+            new_height = int(cur_height * 1.05)
+        elif event.delta == -120:
+            new_width = int(cur_width * 0.95)
+            new_height = int(cur_height * 0.95)
+        else:
+            return
+        # destroy the current image
+        for widget in new_window.winfo_children():
+            widget.destroy()
+        # resize the actual image
+        resized_image = Image.open(file_path).resize((new_width, new_height))
+        new_img = ImageTk.PhotoImage(resized_image)
+        new_window["height"] = new_height
+        new_window["width"] = new_width
+        # render image in the frame of the widget
+        image = tk.Label(new_window, image=new_img)
+        # create a reference for the file
+        image.image = new_img  # type: ignore
+        image.pack()
+
+    def on_click(event) -> None:
+        """
+            Store plot profile endpoints in global variable.
+        """
+        global plot_profile_data
+        global focused_file
+        if focused_file["mode"] != "L":
+            return
+
+        if plot_profile_data["start"] == [-1, -1]:
+            plot_profile_data["start"] = [event.x, event.y]
+
+        elif plot_profile_data["end"] == [-1, -1]:
+            plot_profile_data["end"] = [event.x, event.y]
+
+            plot_profile_button["state"] = "normal"
+        else:
+            plot_profile_data["start"] = [-1, -1]
+            plot_profile_data["end"] = [-1, -1]
+            plot_profile_button["state"] = "disabled"
+
+    # bind certain events to specific functions
     new_window.bind("<FocusIn>", on_focus)
+    new_window.bind("<MouseWheel>", on_scroll)
+    new_window.bind("<Button-1>", on_click)
 
     image = tk.Label(new_window, image=new_img)
     # it has to be a reference, otherwise the image doesn't load!
@@ -70,7 +136,9 @@ def import_image():
 
 
 def sort_histogram_data(data: Dict[str, int]) -> Dict[str, int]:
-
+    """
+    Given the probably not full, unsorted dictionary of values, return a sorted dictionary with "blank" zeroes if value doesn't appear.
+    """
     sorted_data: Dict[str, int] = {}
     for i in range(256):
         key = str(i)
@@ -96,6 +164,9 @@ def plot_histogram(data: Dict[str, int], label: str = "", color: str = "gray") -
 
 
 def generate_histogram_table(data: Dict[str, int]) -> None:
+    """
+    Creates a new widget with histogram data in form of a copyable text.
+    """
     sorted_data = sort_histogram_data(data)
     new_window = tk.Toplevel(root)
     new_window.iconphoto(False, icon)
@@ -110,15 +181,16 @@ def generate_histogram_table(data: Dict[str, int]) -> None:
 
 
 def compose_histogram(mode: str):
+    """
+    Collects data about the image to render later in a histogram. Some aspects of the program are available depending of the image mode (greyscale, RGB, RGBA.)
+    """
     if focused_file == "":
         return
     new_image = Image.open(focused_file["path"])
     pixel_list = list(new_image.getdata())
-    print(new_image.mode)
     match (new_image.mode):
         # L for greyscale images, RGB for color images (duh)
         case 'L':
-            print("This image is greyscale.")
             color_values = {}
             i = 0
             while i < len(pixel_list):
@@ -135,10 +207,8 @@ def compose_histogram(mode: str):
                 plt.show()
             elif mode == 'array':
                 generate_histogram_table(color_values)
-                print('testing')
 
         case 'RGB' | 'RGBA':
-            print("This image is RGB.")
             r_values = {}
             g_values = {}
             b_values = {}
@@ -176,6 +246,28 @@ def compose_histogram(mode: str):
                 plt.show()
 
 
+def plot_profile() -> None:
+
+    global plot_profile_data, focused_file
+    x0, y0 = plot_profile_data["start"][0], plot_profile_data["start"][1]
+    x1, y1 = plot_profile_data["end"][0], plot_profile_data["end"][1]
+    x = np.linspace(
+        x0, x1, 100)
+    y = np.linspace(
+        y0, y1, 100)
+    z = Image.open(focused_file["path"])
+    zi = scipy.ndimage.map_coordinates(z, (y, x))
+
+    fig, axes = plt.subplots(nrows=2)
+    axes[0].imshow(z)
+    axes[0].plot([x0, x1], [y0, y1], 'ro-')
+    axes[0].axis('image')
+
+    axes[1].plot(zi)
+
+    plt.show()
+
+
 import_button = tk.Button(root,
                           text="import image",
                           pady=5,
@@ -203,14 +295,14 @@ histogram_array_button.grid(column=3, row=1, padx=5, pady=5)
 plot_profile_button = tk.Button(root,
                                 text="plot profile",
                                 pady=5,
-                                padx=10, command=lambda: compose_histogram('array'),
+                                padx=10, command=plot_profile,
                                 font=("consolas", 12)
 
                                 )
 plot_profile_button.grid(column=4, row=1, padx=5, pady=5)
 
 
-# disable buttons at the start since there's no file to operate on
+# disable buttons at the start since there's no data to operate on
 histogram_array_button["state"] = "disabled"
 histogram_button["state"] = "disabled"
 plot_profile_button["state"] = "disabled"
